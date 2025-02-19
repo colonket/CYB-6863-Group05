a51e0f3e32604209455d59a8b6a88a4bb02f97dd
Eric Iniguez <eric@iniguez.dev>
2025-02-19 13:48:16 -0600

    Set up structure of preparation.py

--- Scripting_Assignment/preparation.py
+++ Scripting_Assignment/preparation.py
@@ -1,2 +1,33 @@
 #!/usr/bin/env python3
 # Preparation Phase - Asset Inventory Script
+
+def main():
+    get_active_users()
+    get_software_info()
+    get_security_patches()
+    get_auto_runs()
+    get_usb_history()
+
+
+# List all active users on a system
+def get_active_users():
+    pass
+
+# Identify installed software and versions
+def get_software_info():
+    pass
+
+# Check for missing security patches
+def get_security_patches():
+    pass
+
+# List auto runs
+def get_auto_runs():
+    pass
+
+# Identify USB History
+def get_usb_history():
+    pass
+
+if __name__ == __main__:
+    main()
