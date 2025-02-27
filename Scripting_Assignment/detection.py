4e1cbf8b71d6a21f4d277147efb8df8365c2d099
Eric Iniguez <eric-iniguez@utulsa.edu>
2025-02-27 00:28:08 +0000

    Update output formatting, Fix double counting in root login attempts, Extra error checking for root login checking

--- Scripting_Assignment/detection.py
+++ Scripting_Assignment/detection.py
@@ -23,6 +23,7 @@ def main():
 
     if args.block_failed_logins:
         if(args.interval):
+            print(f"[INFO] Blocking failed login attempts every {args.interval} seconds")
             while True:
                 analyze_logs()
                 time.sleep(args.interval)
@@ -30,23 +31,28 @@ def main():
             analyze_logs()
     if args.monitor_root_logins:
         if(args.interval):
+            print(f"[INFO] Monitoring root login attempts every {args.interval} seconds")
             while True:
                 monitor_privileged_logins()
                 time.sleep(args.interval)
         else:
             monitor_privileged_logins()
 
-# Log file path (adjust to your system)
-log_file = "/var/log/auth.log"  # Example: /var/log/auth.log on Linux
-
-# Failed login threshold
-threshold = 5
-time_window = 600  # 10 minutes in seconds
-
 # Dictionary to store failed login attempts (IP: [timestamps])
-failed_attempts = defaultdict(list)
-blocked_ips = set() # IPs blocked by IP tables, because it exceeded threshhold
 def analyze_logs():
+    # Log file path (adjust to your system)
+    log_file = "/var/log/auth.log"  # Example: /var/log/auth.log on Linux
+
+    # Failed login threshold
+    threshold = 5
+    time_window = 600  # 10 minutes in seconds
+
+    total_failed = 0
+    failed_attempts = defaultdict(list)
+    blocked_ips = set() # IPs blocked by IP tables, because it exceeded threshhold
+
+    print(f"[INFO] Reading {log_file} for failed login attempts...")
+
     try:
         with open(log_file, "r") as f:
             for line in f:
@@ -55,47 +61,64 @@ def analyze_logs():
                 if match:
                     ip = match.group("ip")
                     failed_attempts[ip].append(time.time())
+                    total_failed += 1
 
                     # Check for threshold
                     now = time.time()
                     recent_attempts = [t for t in failed_attempts[ip] if now - t < time_window]
                     failed_attempts[ip] = recent_attempts  # Keep only recent attempts
                     if (len(recent_attempts) >= threshold) and (ip not in blocked_ips):
-                        print(f"ALERT: Potential brute-force attack from IP: {ip}")
+                        print(f"[ALERT] Potential brute-force attack from IP: {ip}")
                         # You could add code here to block the IP (Bonus Challenge)
                         # Block the IP (iptables example - requires root privileges)
                         try:
                             subprocess.run(["sudo","iptables", "-A", "INPUT", "-s", ip, "-j", "DROP"], check=True)  # Adapt to your needs
-                            print(f"IP {ip} blocked.")
+                            print(f"[ALERT] Blocked IP: {ip}")
                             blocked_ips.add(ip)
                         except subprocess.CalledProcessError as e:
-                            print(f"Error blocking IP: {e}")
+                            print(f"[ERROR] Could not block IP: {e}")
+        print(f"[INFO] Found {total_failed} failed attempt(s) from {len(failed_attempts)} unique IP address(es)")
 
     except FileNotFoundError:
-        print(f"Error: Log file '{log_file}' not found.")
+        print(f"[ERROR] Log file '{log_file}' not found.")
+        return -1
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"[ERROR] An error occurred: {e}")
+        return -1
 
 def monitor_privileged_logins():
+    print(f"[INFO] Checking audit logs for root login attempts...")
+    root_success = 0
+    root_fail = 0
     try:
         # Run ausearch command to get succesful  root login events
         success_output = subprocess.check_output(['sudo', 'ausearch', '-ui', '0', '-i', '-m', 'USER_LOGIN'],stderr=subprocess.STDOUT).decode('utf-8')
 
         if "<no_matches>" not in success_output:
             for line in success_output.splitlines():
-                if "acct=root" in line:
-                    print(f"Privileged login detected: {line.strip()}")
+                if ("acct=root" in line) and not ("res=failed" in line):
+                    print(f"[ALERT] root login detected: {line.strip()}")
+                    root_success +=1
         else:
-            print("No successful root login events found.")
+            print("[INFO] No successful root login events found.")
                     
         # Run lastb command to get failed root login events and print root lines
         fails_output = subprocess.check_output(['sudo', 'lastb', "|", "grep", "root"], stderr=subprocess.STDOUT).decode('utf-8')
         for line in fails_output.splitlines():
                 if 'root' in line:
-                    print(f"Failed privileged login detected: {line.strip()}")
+                    print(f"[ALERT] Failed privileged login detected: {line.strip()}")
+                    root_fail +=1
+        
+        print(f"[INFO] Found {root_fail+root_success} root login attempt(s)")
+        print(f"[INFO] {root_success} succesful attempt(s), {root_fail} failed attempt(s)")
         
     except subprocess.CalledProcessError as e:
-        print("Error running ausearch: ", e.output.decode('utf-8'))
+        if("ausearch: command not found" in e.output.decode('utf-8')):
+            print("[ERROR] ausearch not found. You may need to install package 'auditd' to use 'ausearch'")
+        elif("<no matches>" in e.output.decode('utf-8')):
+            print("[INFO] No root login attempts detected")
+        else:
+            print("[ERROR] Error running ausearch: ", e.output.decode('utf-8'))
         return
             
 if __name__ == "__main__":
